6ad7ffe45f92f79e9506d293c0252d5d33530525
thale01 <siddhithale01@gmail.com>
2026-04-27 19:12:00 +0530

    Implement dynamic database switching for Render and Local

--- threadfusion/settings.py
+++ threadfusion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,12 +83,17 @@ WSGI_APPLICATION = 'threadfusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if RENDER_EXTERNAL_HOSTNAME:
+    DATABASES = {
+        'default': dj_database_url.parse('postgresql://postgres:Siddhithale01%40@db.zwumacxkvutbmmkatcyy.supabase.co:5432/postgres')
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
