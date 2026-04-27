12fa5e1484393df1ac5db755e84cc1474a52fa89
thale01 <siddhithale01@gmail.com>
2026-04-27 19:05:21 +0530

    Fix DisallowedHost and CSRF settings for Render

--- threadfusion/settings.py
+++ threadfusion/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-default-key-for-dev'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['threadfusion-qs75.onrender.com', 'localhost', '127.0.0.1']
+
+# Dynamic Render Hostname
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}', 'https://threadfusion-qs75.onrender.com']
+else:
+    CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
 
 
 # Application definition
